5e7fbdb3317f1c478566051f5ad4b3458166ee62
imvickykumar999 <imvickykumar999@gmail.com>
2023-02-28 22:32:29 +0530

    cube

---
+++ Tutorials/YouTube Playlist/How to Install Ursina Engine.py
@@ -0,0 +1,7 @@
+
+# https://www.youtube.com/watch?v=w2gu9Ah95l0&list=PLgQYnHnDxgtg-I3m01mGc5wfJwqpT9S3i&index=2
+
+from ursina import *
+
+app = Ursina()
+app.run()
---
+++ Tutorials/YouTube Playlist/Shapes, Colors and Text.py
@@ -0,0 +1,10 @@
+
+# https://www.youtube.com/watch?v=IFORdb2vOWo&list=PLgQYnHnDxgtg-I3m01mGc5wfJwqpT9S3i&index=2
+
+from ursina import *
+
+app = Ursina()
+
+cube = Entity(model="circle", scale=3)
+
+app.run()
