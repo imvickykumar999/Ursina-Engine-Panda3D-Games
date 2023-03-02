e4e95f644110d0a2e543fc4295f827813cbe1124
imvickykumar999 <imvickykumar999@gmail.com>
2023-03-02 17:35:10 +0530

    Update 4 Setting Boundaries for the Movement.py

--- Tutorials/Ursina Engine – 3D Python Game Engine Tutorial/4 Setting Boundaries for the Movement.py
+++ Tutorials/Ursina Engine – 3D Python Game Engine Tutorial/4 Setting Boundaries for the Movement.py
@@ -6,6 +6,10 @@ from ursina import *
 def update():
     global speed, speed2
 
+    cube2.rotation_x += time.dt*107.34
+    cube2.rotation_y -= time.dt*20.7
+    cube2.rotation_z -= time.dt*73.29
+
     cube.rotation_x += time.dt*107.34
     cube.rotation_y -= time.dt*20.7
     cube.rotation_z -= time.dt*73.29
@@ -22,11 +26,13 @@ app=Ursina()
 speed=.6
 cube=Entity(model='cube',
             color=color.red,
+            texture='brick',
             scale=1.4
             )
 speed2=.41
 cube2=Entity(model='sphere',
             color=color.blue,
+            texture='brick',
             # position=(.02,-.1,.03)
             )
 app.run()
