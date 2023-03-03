58b7173de2740eec7c1aaaa8681255ba78e88866
imvickykumar999 <imvickykumar999@gmail.com>
2023-03-03 16:09:09 +0530

    frame of reference changed

--- Tutorials/Practice/Bouncing Sphere.py
+++ Tutorials/Practice/Bouncing Sphere.py
@@ -6,25 +6,28 @@ from ursina import *
 def update():
     global y, speed
 
-    cube.rotation_x += time.dt*50.34
-    cube.rotation_y -= time.dt*20.7
-    cube.rotation_z -= time.dt*73.29
+    cube.rotation_x += time.dt*150.34
+    # cube.rotation_y -= time.dt*120.7
+    # cube.rotation_z -= time.dt*73.29
 
-    y += time.dt*speed*.5
-    print(speed)
+    y += time.dt*speed
+    print(y, speed)
 
-    if abs(y)>3:
-        speed *= -.81
-    
-    camera.position=(0,y,-25)
+    if abs(y) > 3:
+        speed *= -1
+
+    # camera.position=(0,y,-30)
+    cube.y += time.dt*speed
 
 app = Ursina()
 cube = Entity(model="sphere", 
               color=color.red,
-              scale=2.5,
+              scale=2,
               texture='brick',
               )
 
 y=0
-speed=2
+speed=1
+
+# time.sleep(1)
 app.run()
