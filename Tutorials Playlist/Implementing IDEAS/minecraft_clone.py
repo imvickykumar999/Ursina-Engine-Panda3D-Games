9b030ac9bf128f717c6cac4f72e0c2ebefbaf792
imvickykumar999 <imvickykumar999@gmail.com>
2023-03-09 12:20:41 +0530

    Minecraft game

--- Tutorials Playlist/Implementing IDEAS/minecraft_clone.py
+++ Tutorials Playlist/Implementing IDEAS/minecraft_clone.py
@@ -4,24 +4,50 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
 
+opt_texture = [
+    'arrow_down',
+    'arrow_right',
+    'brick',
+    'circle',
+    'circle_outlined',
+    'cobblestone',
+    'cursor',
+    'file_icon',
+    'folder',
+    'grass',
+    'heightmap_1',
+    'horizontal_gradient',
+    'noise',
+    'radial_gradient',
+    'reflection_map_3',
+    'shore',
+    'sky_default',
+    'sky_sunset',
+    'ursina_logo',
+    'ursina_wink_0000',
+    'ursina_wink_0001',
+    'vertical_gradient',
+    'white_cube',
+]
+
 class Voxel(Button):
     def __init__(self, position=(0,0,0)):
         super().__init__(parent=scene,
             position=position,
             model='cube',
             origin_y=.5,
-            texture='white_cube',
-            color=color.color(0, 0, random.uniform(.9, 1.0)),
-            highlight_color=color.lime,
+            texture='brick',
+            highlight_color=color.red,
+            color=color.orange,
         )
 
-for z in range(8):
-    for x in range(8):
+for z in range(15):
+    for x in range(15):
         voxel = Voxel(position=(x,0,z))
 
 def input(key):
     if key == 'left mouse down':
-        hit_info = raycast(camera.world_position, camera.forward, distance=5)
+        hit_info = raycast(camera.world_position, camera.forward, distance=100)
         if hit_info.hit:
             Voxel(position=hit_info.entity.position + hit_info.normal)
     if key == 'right mouse down' and mouse.hovered_entity:
