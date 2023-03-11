77cd9e4f713169f93a18988dd0077191e1d74ea1
imvickykumar999 <imvickykumar999@gmail.com>
2023-03-11 11:23:36 +0530

    csv

--- Tutorials Playlist/Implementing IDEAS/Maze.py
+++ Tutorials Playlist/Implementing IDEAS/Maze.py
@@ -24,6 +24,15 @@ for z in range(n):
     for x in range(n):
         voxel = Voxel(position=(x,0,z))
 
+# https://youtu.be/vX4l-qozib8?list=PLmP1LNMzp97pQe1FiGpdOLKeWYfTMZM7n
+# w=n
+# for i in range(w*w):
+#     bud = Entity(model='cube', color=color.orange,
+#                  texture='brick')
+#     bud.x = i/w
+#     bud.z = i%w
+#     bud.y=1
+
 # brick maze, need better logic to create bhul-bhulaiya !!!
 for y in range(1,3):
     for z in range(0,n,3):
@@ -44,8 +53,8 @@ def input(key):
     if key == 'right mouse down' and mouse.hovered_entity:
         destroy(mouse.hovered_entity)
 
-window.fullscreen = 1
-player = FirstPersonController(gravity=.6)
+# window.fullscreen = 1
+player = FirstPersonController(gravity=1)
 # https://stackoverflow.com/a/75692459/11493297
 
 def update():
@@ -54,3 +63,38 @@ def update():
 
 Sky()
 app.run()
+
+# ---------------------------
+
+# import df_maze as df
+# print(df.returnMaze())
+
+'''
+w   c   w   w   w   w   w   w   w   w   w   w   w
+
+w   c   w   c   w   w   c   w   c   w   c   w   w
+
+w   c   c   c   w   w   c   w   c   w   c   c   w
+
+w   w   w   c   w   c   c   w   c   w   c   w   w
+
+w   w   w   c   w   w   c   c   c   c   c   c   w
+
+w   c   c   c   w   w   w   w   w   c   w   w   w
+
+w   w   w   c   w   c   w   w   c   c   c   c   w
+
+w   c   w   c   w   c   w   c   c   w   c   w   w
+
+w   c   c   c   c   c   w   c   w   w   w   w   w
+
+w   w   w   c   w   c   c   c   c   w   w   c   w
+
+w   w   w   w   w   c   w   w   c   c   w   c   w
+
+w   c   c   c   c   c   c   w   w   c   c   c   w
+
+w   w   w   w   w   w   w   w   w   w   w   c   w
+
+[['w', 'c', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'], ['w', 'c', 'w', 'c', 'w', 'w', 'c', 'w', 'c', 'w', 'c', 'w', 'w'], ['w', 'c', 'c', 'c', 'w', 'w', 'c', 'w', 'c', 'w', 'c', 'c', 'w'], ['w', 'w', 'w', 'c', 'w', 'c', 'c', 'w', 'c', 'w', 'c', 'w', 'w'], ['w', 'w', 'w', 'c', 'w', 'w', 'c', 'c', 'c', 'c', 'c', 'c', 'w'], ['w', 'c', 'c', 'c', 'w', 'w', 'w', 'w', 'w', 'c', 'w', 'w', 'w'], ['w', 'w', 'w', 'c', 'w', 'c', 'w', 'w', 'c', 'c', 'c', 'c', 'w'], ['w', 'c', 'w', 'c', 'w', 'c', 'w', 'c', 'c', 'w', 'c', 'w', 'w'], ['w', 'c', 'c', 'c', 'c', 'c', 'w', 'c', 'w', 'w', 'w', 'w', 'w'], ['w', 'w', 'w', 'c', 'w', 'c', 'c', 'c', 'c', 'w', 'w', 'c', 'w'], ['w', 'w', 'w', 'w', 'w', 'c', 'w', 'w', 'c', 'c', 'w', 'c', 'w'], ['w', 'c', 'c', 'c', 'c', 'c', 'c', 'w', 'w', 'c', 'c', 'c', 'w'], ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'c', 'w']]
+'''
