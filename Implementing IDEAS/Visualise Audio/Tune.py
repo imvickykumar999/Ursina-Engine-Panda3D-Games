207a8f93d028990a9e6ceb1cc45ad7ed977cd83c
imvickykumar999 <imvickykumar999@gmail.com>
2023-04-07 16:01:05 +0530

    winsound inaudible

---
+++ Implementing IDEAS/Visualise Audio/Tune.py
@@ -0,0 +1,34 @@
+
+import winsound
+winsound.PlaySound('input/Imagine Dragons Believer.wav', winsound.SND_FILENAME)	
+
+# freq = 100
+# dur = 50
+# n = 5
+
+# # loop iterates 5 times i.e, 5 beeps will be produced.
+# for i in range(0, n):
+# 	winsound.Beep(freq, dur)
+# 	# freq+= 100
+# 	# dur+= 50
+
+'''
+Consecutive notes with frequency differences of 
+100Hz and time duration 50 milliseconds greater
+than the previous time duration are produced.
+'''
+
+# import winsound         # for sound  
+# import time             # for sleep
+
+# winsound.Beep(440, 250) # frequency, duration
+# time.sleep(0.25)        # in seconds (0.25 is 250ms)
+
+# winsound.Beep(600, 250)
+# time.sleep(0.25)
+
+# ------------------------
+
+# import androidhelper
+# droid=androidhelper.Android()
+# droid.generateDtmfTones('0',100)
